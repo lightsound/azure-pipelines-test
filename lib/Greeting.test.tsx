ca9ffa5b0eed3155d22121f6a2144e9a1da7160c
skoto <0120koto@gmail.com>
2019-02-21 09:00:08 +0900

    test

--- lib/Greeting.test.tsx
+++ lib/Greeting.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-testing-library";
 import Greeting from "./Greeting";
 
-it("renders correctly", () => {
+test("renders correctly", () => {
   const { asFragment } = render(<Greeting text="react" />);
   expect(asFragment()).toMatchSnapshot();
 });
